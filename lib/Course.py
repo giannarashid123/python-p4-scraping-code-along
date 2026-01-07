ead35c7d5fe85ab1e10b24773630c87d2a95abd2
mariamrashid <giannarashid@gmail.com>
2026-01-07 10:07:01 +0300

    Done

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,6 @@
 class Course:
-    pass
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 from Course import Course
-import ipdb
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self, url="http://learn-co-curriculum.github.io/site-for-scraping/courses"):
+        response = requests.get(url)
+        return BeautifulSoup(response.text, 'html.parser')
+
+    def get_courses(self):
+        return self.get_page().select('.post')
+
+    def make_courses(self):
+        for course_element in self.get_courses():
+            title = course_element.select("h2")[0].text if course_element.select("h2") else ''
+            schedule = course_element.select(".date")[0].text if course_element.select(".date") else ''
+            description = course_element.select("p")[0].text if course_element.select("p") else ''
+            
+            new_course = Course(title, schedule, description)
+            self.courses.append(new_course)
+        
+        return self.courses
+
